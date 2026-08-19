cba6b67af456e624ec96caa801cb2523ada0c0af
adithya <adithyas2988@gmail.com>
2026-08-19 22:08:36 +0530

    vowcount

---
+++ python/vowcount.py
@@ -0,0 +1,6 @@
+word=input("enter a word: ")
+print("no of vowels in the word is :", word.count("a")+
+      word.count("e")+
+       word.count("i")+
+        word.count("o")+
+        word.count("u")) 
